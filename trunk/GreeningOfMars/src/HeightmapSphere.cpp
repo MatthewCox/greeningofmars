f2246bf8685a7b3dccf0ff9cb32aaa25ee7915d9
tyranicmoron <tyranicmoron@e014f330-dd33-11de-81ce-5f215e6f531d>
2010-03-04 21:53:07 +0000

    Mmm, textured Mars

--- trunk/GreeningOfMars/src/HeightmapSphere.cpp
+++ trunk/GreeningOfMars/src/HeightmapSphere.cpp
@@ -6,16 +6,27 @@
 
 HeightmapSphere::HeightmapSphere(void)
 {
+	texture = new glTexture;
+	texLoader = new TextureLoader();
+	texLoader->LoadTextureFromDisk(".\\MarsHeight.bmp", texture);
+	quadric = gluNewQuadric();
 }
 
 HeightmapSphere::~HeightmapSphere(void)
 {
+	texLoader->FreeTexture(texture);
+	delete texLoader;
+	gluDeleteQuadric(quadric);
 }
 
 void HeightmapSphere::Draw()
 {
-	glColor4f(0.5f, 0.2f, 0.05f, 1.0f);
-	GLUquadric* quadric = gluNewQuadric();
-	gluSphere(quadric, 5, 18, 18);
-	gluDeleteQuadric(quadric);
+	glPushMatrix();
+		glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		gluQuadricTexture(quadric, GL_TRUE);
+		glBindTexture(GL_TEXTURE_2D, texture->TextureID);
+		glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+		gluSphere(quadric, 5, 36, 36);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	glPopMatrix();
 }
